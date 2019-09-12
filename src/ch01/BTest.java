a1bf9900a725ab79c6eac34093f09678e11324b3
ksm05 <ksm05@DESKTOP-4I2FD8K>
2019-09-12 16:27:57 +0900

    깃허브 마스터에 수정됨

--- src/ch01/BTest.java
+++ src/ch01/BTest.java
@@ -3,6 +3,13 @@ package ch01;
 public class BTest {
 	public static void main(String[] args) {
 		System.out.println("시험시험1");
+		System.out.println("시험시험1");
+		System.out.println("시험시험1");
+		if(true) {
+
+			System.out.println("시험시험1");
+			System.out.println("시험시험1");
+		}
 		System.out.println("시험시험2 ");
 	}
 }
--- src/ch01/HelloGit.java
+++ src/ch01/HelloGit.java
@@ -3,6 +3,11 @@ package ch01;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("HellowGit~~~~");
+
+		System.out.println("HellowGit~~~~");
+		
+		
+		
 		System.out.println("HellowGit~~~~");
 		
 	}										
