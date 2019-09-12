9d0461f88c614473972178d8fc4f07d1444c96d7
dmdkrjems <53504245+dmdkrjems@users.noreply.github.com>
2019-09-12 16:30:03 +0900

    주석을추가

--- src/ch01/BTest.java
+++ src/ch01/BTest.java
@@ -6,8 +6,10 @@ public class BTest {
 		System.out.println("시험시험1");
 		System.out.println("시험시험1");
 		if(true) {
-
+				//주석을 추가했다
 			System.out.println("시험시험1");
+			
+			//깃허브주석달기
 			System.out.println("시험시험1");
 		}
 		System.out.println("시험시험2 ");
